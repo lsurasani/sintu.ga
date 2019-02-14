51c00eb8974eed04dfff7c61077d8171b5e23ee5
Sintayehu <sintemite@gmail.com>
2019-02-13 19:44:26 -0500

    second commit

---
+++ sprint-02/app.js
@@ -0,0 +1,13 @@
+// alert('Welcome to my website, please visit all my pages and leave your feedback on the contact page!');
+
+
+// function myFunction() {
+//   var btn = "Click here for Details!";
+//   var result= btn.link("http://www.holiday-to-ethiopia.com/cultural-tour-to-ethiopia/festival-tours-ethiopia/item/124-meskel-festival-with-ethiopia-s-ancient-civilization-culture");
+//   document.getElementById("detail").innerHTML = result;
+// };
+
+
+
+
+
